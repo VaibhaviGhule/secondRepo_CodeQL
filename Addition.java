79c917d170ff15270a582b6c5554a0851d9034c4
Vaibhavi Ghule <vaibhavi.ghule@sarathisoftech.com>
2024-01-25 17:25:19 +0530

    CodeQL_changes

---
+++ Addition.java
@@ -0,0 +1,10 @@
+package secondRepo_CodeQL;
+
+public class Addition
+{
+    public static void main(String args[])
+    {
+        int a=5,b=9,c=a+b;
+        System.out.println("secondRepo_CodeQL.Addition is  : "+c);
+    }
+}
